e7b8b58a991b547bd29ed074e4d2458cf37116c6
Hershal Bhave <hershal.bhave@gmail.com>
2015-05-26 14:42:48 -0500

    distance-projection: added, in progress

---
+++ tests/distance-projection/distance-projection.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "node.hpp"
+
+int main() {
+
+    ik::node* node0 = new ik::node(0, 0, 0);
+    ik::node* node1 = new ik::node(1, 1, 1);
+
+
+    std::cout << "Hello World" << std::endl;
+}
+
