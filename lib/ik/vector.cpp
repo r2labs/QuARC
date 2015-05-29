5ed19d7aac0692110cf96e2f2de632ddf9420c82
Hershal Bhave <hershal.bhave@gmail.com>
2015-05-28 19:23:52 -0500

    vector: removed another const
    Will added consts back in when I'm done

--- lib/ik/vector.cpp
+++ lib/ik/vector.cpp
@@ -10,10 +10,11 @@ ik::vector::vector(ik::position* origin,
                    float off_z)
     : origin(origin), off_x(off_x), off_y(off_y), off_z(off_z) {}
 
-const float ik::vector::magnitude() {
 ik::vector::~vector() {
     delete origin;
 }
+
+float ik::vector::magnitude() {
     return sqrt(pow(get_off_x(), 2) +
                 pow(get_off_y(), 2) +
                 pow(get_off_z(), 2));
