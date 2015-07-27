586eed680154d53ecd0c4858abd56f56721bd5df
William Mauldin <stormymauldin@utexas.edu>
2015-07-27 13:03:40 -0500

    This is the class to control the motor via the PWM board.
    I am not familiar with the OS for the PWM board, so the moveToAngle() function is not complete. It looks up the pulse width in the table but does not output it.

---
+++ resources/motorControl.cpp
@@ -0,0 +1,33 @@
+#include <math.h>
+#include "motorControl.hpp"
+
+
+pwm::motorControl::motorControl(){}
+
+pwm::motorControl::motorControl(float angle, int angleType)
+{
+	if(angleType == true)
+	{
+		angleRadians = angle;
+	}
+	else
+	{
+		angleRadians = angle*PI/180;
+	}
+}
+
+pwm::motorControl::moveToAngle()
+{
+	if(angleRadians > MAX_ANGLE_RADIANS || angleRadians < MIN_ANGLE_RADIANS)
+	{
+		return false;
+	}
+	else
+	{
+		int index = angleRadians*45
+		int pulseWidth = pulseWidths[index];
+		/*OUTPUT PULSE WIDTH*/
+		/*I am not familiar with the OS for the PWM, so I am not sure what lines are needed for this*/
+		return true;
+	}
+}
