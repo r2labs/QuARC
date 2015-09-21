46ec5fb97d1d10244748fc34c52311b8d66ba03b
William Mauldin <stormymauldin@utexas.edu>
2015-09-21 12:48:05 -0500

    changed degrees -> radians to radians -> degrees. Need to update conversion method.

--- resources/motorControl.cpp
+++ resources/motorControl.cpp
@@ -4,27 +4,28 @@
 
 pwm::motorControl::motorControl(){}
 
+//angleType == true means that the input angle is in degrees
 pwm::motorControl::motorControl(float angle, int angleType)
 {
 	if(angleType == true)
 	{
-		angleRadians = angle;
+		angleDegrees = angle;
 	}
 	else
 	{
-		angleRadians = angle*PI/180;
+		angleDegrees = angle*180/PI;
 	}
 }
 
 pwm::motorControl::moveToAngle()
 {
-	if(angleRadians > MAX_ANGLE_RADIANS || angleRadians < MIN_ANGLE_RADIANS)
+	if(angleDegrees > 180 || angleDegrees < 0)
 	{
 		return false;
 	}
 	else
 	{
-		int index = angleRadians*45	//this conversion probably needs to be modified.
+		int index = angleDegrees*45	//this conversion probably needs to be modified.
 		int pulseWidth = pulseWidths[index];
 		/*OUTPUT PULSE WIDTH*/
 		/*I am not familiar with the OS for the PWM, so I am not sure what lines are needed for this*/
